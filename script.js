3cee0ce89b670cf24f999a899e2a17b1a1e0f8ab
Jesuisleon <frederic-versavel_student2022@wilder.school>
2023-03-24 23:27:50 +0100

    Init the project

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Fonction pour le Toggle Button
 function toggleButton(value) {
-    let label = document.querySelector('label[for="'+value.name+'"]')
+    let label = document.querySelector(`label[for="${value.name}"]`)
     if (value.checked) {
         label.innerText = "LIRE MOINS";
     } else {
@@ -14,13 +14,12 @@ const burger = document.querySelector('.burger');
 function toggleMenu(){  
     burger.addEventListener('click' ,()=>{
         navbar.classList.toggle('showmenu');
-        // navbar.style.backgroundColor = 'transparent';
     })
     
 }
 toggleMenu();
 
 function toggleLink(){
-    const navbar = document.querySelector(`.navbar`);
+    const navbar = document.querySelector(".navbar");
     navbar.classList.remove("showmenu");
 }
